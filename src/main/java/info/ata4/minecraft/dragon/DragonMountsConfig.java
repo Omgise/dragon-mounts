7590530224be96796cdcdb98c54c4ffd5c301449
jss2a98aj <jsimmons3959@neo.rr.com>
2021-01-25 01:19:27 -0500

    Improve some config option descriptions

--- src/main/java/info/ata4/minecraft/dragon/DragonMountsConfig.java
+++ src/main/java/info/ata4/minecraft/dragon/DragonMountsConfig.java
@@ -17,39 +17,37 @@ import net.minecraftforge.common.config.Configuration;
  */
 public class DragonMountsConfig {
 
-	private static final String categoryDebug = "debug";
-	private static final String categoryFoods = "dragonFoods";
-	private static final String categoryGrowth = "dragonGrowthTimes";
+	private static final String categoryFood = "dragon food";
+	private static final String categoryGrowth = "dragon growth";
 	private static final String categoryOther = "other";
 	
 
     // config properties
-    public static boolean eggsInChests = false;
-    public static int dragonEntityID = -1;
-
-    public static boolean registerMeatToOreDictionary = true;
+	public static String dragonAllFoodTag = "listAllmeatraw";
     public static String dragonFavoriteFoodTag = "listAllfishraw";
-    public static String dragonAllFoodTag = "listAllmeatraw";
+    public static boolean registerMeatToOreDictionary = true;
 
     public static int eggToHatchlingTime = 72000;
     public static int hatchlingToJuvenileTime = 48000;
     public static int juvenileToAdultTime = 24000;
 
     public static boolean debug = false;
+    public static int dragonEntityID = -1;
+    public static boolean eggsInChests = false;
 
     public static void LoadDragonMountsConfig(Configuration config) {
-        debug = config.getBoolean("debug", categoryDebug, debug, "Debug mode. Unless you're a developer or are told to activate it, you don't want to set this to true.");
 
-        registerMeatToOreDictionary = config.getBoolean("registerMeatToOreDictionary", categoryFoods, registerMeatToOreDictionary, "Registers meats to the ore dictionary.\nUsesless if non-default values are assigned to dragonFavoriteFoodTag and dragonAllFoodTag");
-        dragonFavoriteFoodTag = config.getString("dragonFavoriteFoodTag", categoryFoods, dragonFavoriteFoodTag, "Items with the specified Ore Dictionary tag are considered favorite foods.");
-        dragonAllFoodTag = config.getString("dragonAllFoodTag", categoryFoods, dragonAllFoodTag, "Items with the specified Ore Dictionary tag are considered edible foods.");
+    	dragonAllFoodTag = config.getString("dragonAllFoodTag", categoryFood, dragonAllFoodTag, "Items with the specified Ore Dictionary tag are considered edible foods.");
+        dragonFavoriteFoodTag = config.getString("dragonFavoriteFoodTag", categoryFood, dragonFavoriteFoodTag, "Items with the specified Ore Dictionary tag are considered favorite foods.");
+        registerMeatToOreDictionary = config.getBoolean("registerMeatToOreDictionary", categoryFood, registerMeatToOreDictionary, "Registers meats to the ore dictionary.\nUsesless if non-default values are assigned to dragonFavoriteFoodTag and dragonAllFoodTag");
 
-        eggToHatchlingTime = config.getInt("eggToHatchlingTime", categoryGrowth, eggToHatchlingTime, 4800, Integer.MAX_VALUE, "How long an egg takes to hatch into a hatchling.");
-        hatchlingToJuvenileTime = config.getInt("hatchlingToJuvenileTime", categoryGrowth, hatchlingToJuvenileTime, 4800, Integer.MAX_VALUE, "How long a hatchling takes to mature into a juvenile.");
-        juvenileToAdultTime = config.getInt("juvenileToAdultTime", categoryGrowth, juvenileToAdultTime, 4800, Integer.MAX_VALUE, "How long a juvenile takes to mature into an adult.");
+        eggToHatchlingTime = config.getInt("eggToHatchlingTime", categoryGrowth, eggToHatchlingTime, 4800, Integer.MAX_VALUE, "How many ticks eggs take to turn into hatchlings.\nThere are 20 ticks in a second and 24000 in a Miecraft day.");
+        hatchlingToJuvenileTime = config.getInt("hatchlingToJuvenileTime", categoryGrowth, hatchlingToJuvenileTime, 4800, Integer.MAX_VALUE, "How many ticks hatchlings take to turn into juveniles.\nThere are 20 ticks in a second and 24000 in a Miecraft day.");
+        juvenileToAdultTime = config.getInt("juvenileToAdultTime", categoryGrowth, juvenileToAdultTime, 4800, Integer.MAX_VALUE, "How many ticks juveniles takes to turn into adults.\nThere are 20 ticks in a second and 24000 in a Miecraft day.");
 
-        eggsInChests = config.getBoolean("eggsInChests", categoryOther, eggsInChests, "Spawns dragon eggs in generated chests when enabled.");
+        debug = config.getBoolean("debug", categoryOther, debug, "Debug mode. Unless you're a developer or are told to activate it, you don't want to set this to true.");
         dragonEntityID = config.getInt("dragonEntityID", categoryOther, dragonEntityID, -1, 255, "Overrides the entity ID for dragons to fix problems with manual IDs from other mods.\nSet to -1 for automatic assignment (recommended).\nWarning: wrong values may cause crashes and loss of data!");
+        eggsInChests = config.getBoolean("eggsInChests", categoryOther, eggsInChests, "Spawns dragon eggs in generated chests when enabled.");
 
         if (config.hasChanged()) {
             config.save();
